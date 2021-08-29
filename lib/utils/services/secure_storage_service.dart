ed8802317efed2782da7ee4d5e65315ec0d9ac2d
Jose Bejarano <jabcarranz@gmail.com>
2021-08-28 21:30:43 -0500

    Added Secure Storage service class

--- lib/utils/services/secure_storage_service.dart
+++ lib/utils/services/secure_storage_service.dart
@@ -2,5 +2,25 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorage {
 
-  final storage = new FlutterSecureStorage();
+  final FlutterSecureStorage _storage;
+
+  SecureStorage({FlutterSecureStorage? storage})
+    : _storage = storage ?? FlutterSecureStorage() ;
+
+  Future<String?> getValue(String key) async {
+    return await _storage.read(key: key);
+  }
+
+  Future<void> setValue(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<void> deleteValue(String key) async {
+    await _storage.delete(key: key);
+  }
+
+  Future<void> cleanStorage() async{
+    await _storage.deleteAll();
+  }
+
 }
