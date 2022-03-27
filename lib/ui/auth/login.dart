17682fc7753d28270dfe4ece0a35da26047448bb
Jose Bejarano <jose@caffelli.com>
2022-03-26 21:47:58 -0500

    Fixed social buttons and added splash screen

--- lib/ui/auth/login.dart
+++ lib/ui/auth/login.dart
@@ -11,6 +11,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           child: Padding(
@@ -18,7 +19,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 48.0, bottom: 48.0),
+                  padding: const EdgeInsets.symmetric(vertical: 56.0),
                   child: Image.asset('assets/images/logo.png'),
                 ),
                 Padding(
@@ -65,68 +66,83 @@ class _LoginPageState extends State<LoginPage> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        height: 60.0,
-                        width: 100.0,
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12.0),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0,1),
-                              blurRadius: 3.0,
-                              color: Colors.black.withOpacity(0.3)
-                            )
-                          ] 
+                      // GestureDetector(
+                      //   onTap: () => {print("Container clicked")},
+                      //   child: Container(
+                      //     height: 60.0,
+                      //     width: 100.0,
+                      //     padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      //     decoration: BoxDecoration(
+                      //       color: Colors.white,
+                      //       borderRadius: BorderRadius.circular(12.0),
+                      //       boxShadow: [
+                      //         BoxShadow(
+                      //           offset: Offset(0,1),
+                      //           blurRadius: 3.0,
+                      //           color: Colors.black.withOpacity(0.3)
+                      //         )
+                      //       ] 
+                      //     ),
+                      //     child: Image.asset(
+                      //       'assets/images/social/google.png',
+                      //       scale: 18.0,
+                      //     ),
+                      //   ),
+                      // ),
+                      ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          minimumSize: Size(100.0, 60.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                          )
                         ),
                         child: Image.asset(
                           'assets/images/social/google.png',
-                          scale: 18.0,
+                          scale: 18.0
                         ),
                       ),
-                      Container(
-                        height: 60.0,
-                        width: 100.0,
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12.0),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0,1),
-                              blurRadius: 3.0,
-                              color: Colors.black.withOpacity(0.3)
-                            )
-                          ] 
+                      ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          minimumSize: Size(100.0, 60.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                          )
                         ),
                         child: Image.asset(
                           'assets/images/social/facebook.png',
-                          scale: 18.0,
+                          scale: 18.0
                         ),
                       ),
-                      Container(
-                        height: 60.0,
-                        width: 100.0,
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12.0),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0,1),
-                              blurRadius: 3.0,
-                              color: Colors.black.withOpacity(0.3)
-                            )
-                          ] 
+                      ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          minimumSize: Size(100.0, 60.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)
+                          )
                         ),
                         child: Image.asset(
                           'assets/images/social/twitter.png',
-                          scale: 18.0,
+                          scale: 18.0
                         ),
-                      )
+                      ),
+                      
+                      
                     ],
                   ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text('Not registered yet? click here'),
+                    ),
+                  ) 
                 )
               ],
             ),
